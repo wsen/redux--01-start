fd311ea6b2fb41f9d970dfebaf85e3433b3413cf
w_sen <w_sen@flashbox.de>
2018-02-03 21:52:18 +0100

    birth(local): Setting Up Reducer and Store

---
+++ redux-basics.js
@@ -0,0 +1,15 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
+// Reducer
+const rootReducer = (state, action) => {
+    return state;
+}
+
+// Store
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+// Dispatching Action
+
+// Subscription
