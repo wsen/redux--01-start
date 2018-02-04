6ec128d46690b54d93d70d275d2d2fe640ad0966
w_sen <w_sen@flashbox.de>
2018-02-04 20:41:12 +0100

    task(local): Redux Introduction Assignment: Same Procedure extended

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,10 +3,33 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-    if (action.type === 'INCREMENT') {
-        return {
-            counter: state.counter + 1
-        }
+
+    switch ( action.type ) {
+        case ( 'INCREMENT' ):
+            return {
+                counter: state.counter + 1
+            }
+            break;
+        case ( 'DECREMENT' ):
+            return {
+                counter: state.counter - 1
+            }
+            break;
+        case ( 'ADD' ):
+            return {
+                counter: state.counter + 5
+            }
+            break;
+        case ( 'SUBTRACT' ):
+            return {
+                counter: state.counter - 5
+            }
+            break;
+
+        // if (action.type === 'INCREMENT') {
+        //     return {
+        //         counter: state.counter + 1
+        //}
     }
     return state;
 };
