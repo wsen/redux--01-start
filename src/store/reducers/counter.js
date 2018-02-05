85340a246e0d9cbe1a0acd776523162fdd91a508
w_sen <w_sen@flashbox.de>
2018-02-05 22:23:24 +0100

    chore(local): 249 Combining Multiple Reducers

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,12 +1,13 @@
+import * as actionTypes from '../actions';
+
 const initialState = {
-    counter: 0,
-    results: []
+    counter: 0
 }
 
 const reducer = (state = initialState, action) => {
 
     switch ( action.type ) {
-        case ( 'INCREMENT' ):
+        case actionTypes.INCREMENT:
             // const newState = Object.assign({}, state);
             // newState.counter: state.counter + 1;
             // return newState;
@@ -14,34 +15,20 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 counter: state.counter +1
             }
-        case ( 'DECREMENT' ):
+        case actionTypes.DECREMENT:
             return {
                 ...state,
                 counter: state.counter - 1
             }
-        case ( 'ADD' ):
+        case actionTypes.ADD:
             return {
                 ...state,
                 counter: state.counter + action.val
             }
-        case ( 'SUBTRACT' ):
+        case actionTypes.SUBTRACT:
             return {
                 ...state,
                 counter: state.counter - action.val
-            }
-        case 'STORE_RESULT':
-            return {
-                ...state,
-                results: state.results.concat({id: new Date(), value: state.counter})
-            }
-        case 'DELETE_RESULT':
-            // const id = 2;
-            // const newArray = [...state.results]
-            // state.results.splice(id, 1)
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            return {
-                ...state,
-                results: updatedArray
             }
     }
     return state;
